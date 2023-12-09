5b5054c7674cbc8657038fd93ca0c7a02627ae5c
flume08 <carreno.luis@gmail.com>
2023-12-09 15:53:46 -0400

    first component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-
+import AddTask from "components/AddTask"
 export default function Home() {
   return (
   <main className="max-w-4xl mx-auto mt-4">
-    <div>
-      hello
+    <div className='text-center my-5 flex flex-col gap-4'>
+      <h1 className='text-2xl font-bold'></h1>
+      <AddTask />
     </div>
   </main>
   )
